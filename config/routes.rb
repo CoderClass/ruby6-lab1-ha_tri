4c0efe13fc38598f9be877d6fd680a629efc6c5c
Tri Ngo <ngominhtrint@gmail.com>
2017-06-14 20:02:35 +0700

    Milestone 3: The PhotosController and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :photos
+  
+  get 'photos/index'
+
+  get 'photos/show'
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ vendor/gems/ffi-1.9.14/ext/ffi_c/libffi-x86_64-darwin15/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/tringo/Documents/Projects/Rails/lab_one/vendor/gems/ffi-1.9.14/ext/ffi_c/libffi/src/x86/ffitarget.h
